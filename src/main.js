09cff8e3dda4f5b4fdff559ecd64b77f87a977e7
Владимир <anfalow_w@rambler.ru>
2022-01-23 22:50:23 +0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import titlePlugin from '@/utils/title.plugin'
+import Loader from '@/components/app/Loader'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(titlePlugin)
+Vue.use(Vuelidate)
+Vue.use(VueMeta)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCzpii-po0_ROAVHkmhMHRyB2QobnY2QmA',
+  authDomain: 'calculation-33841.firebaseapp.com',
+  databaseURL: 'https://calculation-33841-default-rtdb.firebaseio.com',
+  projectId: 'calculation-33841',
+  storageBucket: 'calculation-33841.appspot.com',
+  messagingSenderId: '408151063102',
+  appId: '1:408151063102:web:27d8db910d7d0bdc8a2f6f',
+  measurementId: 'G-WWF882Q5RC'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
